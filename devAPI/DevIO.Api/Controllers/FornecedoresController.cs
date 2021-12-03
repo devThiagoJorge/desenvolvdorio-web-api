058c4c72ffb04a050633f32b267f677833ed4b36
Thiago Jorge <thiago.v.jorge@hotmail.com>
2021-12-02 22:23:43 -0300

    adicionado controller e automapper

---
+++ devAPI/DevIO.Api/Controllers/FornecedoresController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using DevIO.Data.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/fornecedores")]
+    public class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository _repository;
+        private readonly IMapper _mapper;
+        public FornecedoresController(IFornecedorRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FornecedorViewModel>>> GetFornecedores()
+        {
+            var fornecedores = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _repository.ObterTodos());
+
+            return Ok(fornecedores);
+        }
+    }
+}
--- devAPI/DevIO.Api/Startup.cs
+++ devAPI/DevIO.Api/Startup.cs
@@ -1,15 +1,10 @@
+using DevIO.Api.Configuration;
+using DevIO.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevIO.Api
 {
@@ -25,8 +20,16 @@ namespace DevIO.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MeuDbContext>(opt =>
+            {
+                
+            });
+
+            services.AddAutoMapper(typeof(Startup));
 
             services.AddControllers();
+
+            services.ResolveDepencencies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
